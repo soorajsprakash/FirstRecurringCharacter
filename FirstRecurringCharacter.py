540a1df6b4fd2041b03b3f024f2a47140a337924
soorajsprakash <soorajsprakash007@gmail.com>
2018-10-05 14:00:51 +0530

    Initial commit

---
+++ FirstRecurringCharacter.py
@@ -0,0 +1,13 @@
+def first_recurring(given_string):
+    counts = {}  # initialise a dictionary
+    for char in given_string:
+        if char in counts:
+            print(char)
+            return char
+        else:
+            counts[char] = 1
+
+
+my_string = input("Enter your string:  ")
+first_recurring(my_string)
+
